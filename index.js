cc90662882004477a0b5f49c2b308d434ebb932a
robertnjunge <mubearobert@gmail.com>
2023-07-12 14:47:37 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Code your solution in this file!
+const companyName = "Scuber"; //defining companyName using const
+
+let mostProfitableNeighborhood = "Chelsea"; // defining mostProfitableNeighborhood using let
+
+let companyCeo = "Susan Smith";
